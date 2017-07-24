0be9ec1b6e95311793d09188f95b5ae05466928d
Solange Harmonie PICARD <solange.picard@cryptolab.net>
2017-07-24 17:25:42 +0200

    Ajout d'une page billet.php pour l'affichage individuel de chaque billet et des commentaires associés, création d'une nouvelle table commentaires et de ses champs, réorganisation de la table billets.

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <?php //Affichage
 //Gestion du nombre limite de billets par page
   $billets_par_page = 10;
-  $retour_total = $bdd->query('SELECT id FROM billets');
+  $retour_total = $bdd->query('SELECT id_billet FROM billets');
   $donnees_total = $retour_total->RowCount();
   $nombre_de_pages = ceil($donnees_total/$billets_par_page); //On compte le nombre de pages.
   if(isset ($_GET['page']) && !empty($_GET['page']) && $_GET['page'] >0 ){
@@ -15,13 +15,14 @@
   }
   $premiere_entree = ($page_courante-1)*$billets_par_page; // On calcule la première entrée à lire
 //Affichage de 10 billets
-  $reponse = $bdd->query('SELECT * FROM billets ORDER BY ID DESC LIMIT '.$premiere_entree.', '.$billets_par_page); // On récupère tout le contenu de la table billets, on affiche les 10 derniers messages par ordre anté-chronologique
+  $reponse = $bdd->query('SELECT * FROM billets ORDER BY id_billet DESC LIMIT '.$premiere_entree.', '.$billets_par_page); // On récupère tout le contenu de la table billets, on affiche les 10 derniers messages par ordre anté-chronologique
   while ($donnees = $reponse->fetch()){ // On affiche chaque entrée une à une
-    echo "N° : " . $donnees['id'] . "<br>";
-    echo "Auteur : " . $donnees['auteur']."<br>";
-    echo "Titre : " . $donnees['titre']."<br>";
-    echo "Date : " . $donnees['date']."<br>";
-    echo "Billet :<br> \"" . $donnees['contenu']."\" <br><br>";
+    echo "N° : " . htmlspecialchars($donnees['id_billet']) . " - ";
+    echo "<a href='billet.php?billet=" . $donnees['id_billet'] . "'>Voir les commentaires</a><br><br>";
+    echo "Auteur : " . htmlspecialchars($donnees['auteur_billet'])."<br>";
+    echo "Titre : " . htmlspecialchars($donnees['titre_billet'])."<br>";
+    echo "Date : " . htmlspecialchars($donnees['date_billet'])."<br>";
+    echo "Billet :<br>" . $donnees['contenu_billet']."<br>";
   }
 //Affichage de la page en cours et de la navigation vers les autres pages
 echo "Page(s) : ";
@@ -30,6 +31,7 @@ echo "Page(s) : ";
       echo  $i.' ';
   } else {
     echo '<a href ="index.php?page='.$i.'">'.$i.'&nbsp;</a>';}
-  }
+  }// Fin de la boucle des billets
+$reponse->closeCursor();
 ?>
 <?php include("includes/footer.php"); ?>
--- publier.php
+++ publier.php
@@ -1,22 +1,22 @@
 <?php include("includes/header.php"); ?>
     <form action="publier.php" method="post">
       <p>
-        <label for="auteur" style="cursor:pointer;">Votre pseudo : </label><br>
-        <input type="text" id="auteur" name="auteur"><br><br>
-        <label for="titre" style="cursor:pointer;">Titre du billet : </label><br>
-        <input type="text" id="titre" name="titre"><br><br>
-        <label for="contenu" style="cursor:pointer;">Votre texte : </label><br>
-        <textarea id="contenu" name="contenu"></textarea><br><br>
+        <label for="auteur_billet" style="cursor:pointer;">Votre pseudo : </label><br>
+        <input type="text" id="auteur_billet" name="auteur_billet"><br><br>
+        <label for="titre_billet" style="cursor:pointer;">Titre du billet : </label><br>
+        <input type="text" id="titre_billet" name="titre_billet"><br><br>
+        <label for="contenu_billet" style="cursor:pointer;">Votre texte : </label><br>
+        <textarea id="contenu_billet" name="contenu_billet"></textarea><br><br>
         <input type="submit" value="Envoyer">
       </p>
     </form>
 
 <?php //Insertion dans la base de données
-  if(isset($_POST['auteur']) && !empty($_POST['auteur']) && isset($_POST['titre']) && !empty($_POST['titre']) && isset($_POST['contenu']) && !empty($_POST['contenu'])) {
-    if(isset($_POST['auteur']) && !empty($_POST['auteur'])){  $auteur = htmlspecialchars($_POST['auteur']); } else { echo "<p style='color:red'>Écrivez votre nom.</p>"; }
-    if(isset($_POST['titre']) && !empty($_POST['titre'])){  $titre = htmlspecialchars($_POST['titre']); } else { echo "<p style='color:red'>Écrivez un titre.</p>"; }
-    if(isset($_POST['contenu']) && !empty($_POST['contenu'])){  $contenu = $_POST['contenu']; } else { echo "<p style='color:red'>Écrivez votre texte.</p>"; }
-    $requete = $bdd->prepare('INSERT INTO billets(auteur, titre, contenu, date) VALUES(?,?,?,now())');
+  if(isset($_POST['auteur_billet']) && !empty($_POST['auteur_billet']) && isset($_POST['titre_billet']) && !empty($_POST['titre_billet']) && isset($_POST['contenu_billet']) && !empty($_POST['contenu_billet'])) {
+    if(isset($_POST['auteur_billet']) && !empty($_POST['auteur_billet'])){  $auteur = htmlspecialchars($_POST['auteur_billet']); } else { echo "<p style='color:red'>Écrivez votre nom.</p>"; }
+    if(isset($_POST['titre_billet']) && !empty($_POST['titre_billet'])){  $titre = htmlspecialchars($_POST['titre_billet']); } else { echo "<p style='color:red'>Écrivez un titre.</p>"; }
+    if(isset($_POST['contenu_billet']) && !empty($_POST['contenu_billet'])){  $contenu = $_POST['contenu_billet']; } else { echo "<p style='color:red'>Écrivez votre texte.</p>"; }
+    $requete = $bdd->prepare('INSERT INTO billets(auteur_billet, titre_billet, contenu_billet, date_billet) VALUES(?,?,?,now())');
     $requete->execute(array($auteur,$titre,$contenu));
     echo "<p style='color:green'>Votre billet a été publié. </p>";
   }
